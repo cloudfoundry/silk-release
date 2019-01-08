663c5dc0f39ae96badcb932d5e06457f6faecd6e
Amelia Downs <adowns@pivotal.io>
2019-01-08 16:46:08 -0500

    fix windows test script
    we should be inside silk-release when running ginkgo
    
    [#160927901]
    
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>

--- scripts/test-windows.ps1
+++ scripts/test-windows.ps1
@@ -1,15 +1,16 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-$env:GOPATH=$PWD
-
-go get github.com/onsi/ginkgo/ginkgo
-
 # Force system temp dir to be on the ephemeral disk
 if (Test-Path env:EPHEMERAL_DISK_TEMP_PATH) {
   mkdir "$env:EPHEMERAL_DISK_TEMP_PATH" -ea 0
   $env:TEMP = $env:TMP = $env:GOTMPDIR = $env:EPHEMERAL_DISK_TEMP_PATH
 }
+
+cd silk
+
+$env:GOPATH=$PWD
+go get github.com/onsi/ginkgo/ginkgo
 
 & "$env:GOPATH/bin/ginkgo.exe" -nodes $env:NODES -r -race -keepGoing `
   -randomizeSuites -skipPackage linux src/vxlan-policy-agent
