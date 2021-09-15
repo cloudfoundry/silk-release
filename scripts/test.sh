54ec0cb059a012b693f8c5ecee6e09d55fb70ad5
Geoff Franks <gfranks@vmware.com>
2021-09-15 13:58:32 +0000

    Test that we dont' really need db tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,39 +16,7 @@ declare -a windows_packages=(
     "src/code.cloudfoundry.org/vxlan-policy-agent/integration/windows"
     )
 
-function bootDB {
-  db=$1
-
-  if [ "$db" = "postgres" ]; then
-    launchDB="(docker-entrypoint.sh postgres &> /var/log/postgres-boot.log) &"
-    testConnection="psql -h localhost -U postgres -c '\conninfo' &>/dev/null"
-  elif [ "$db" = "mysql" ]  || [ "$db" = "mysql-5.6" ]; then
-    launchDB="(MYSQL_ROOT_PASSWORD=password /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
-    testConnection="echo '\s;' | mysql -h 127.0.0.1 -u root --password='password' &>/dev/null"
-  else
-    echo "skipping database"
-    return 0
-  fi
-
-  echo -n "booting $db"
-  eval "${launchDB}"
-  for _ in $(seq 1 60); do
-    set +e
-    eval "${testConnection}"
-    exitcode=$?
-    set -e
-    if [ ${exitcode} -eq 0 ]; then
-      echo "connection established to $db"
-      return 0
-    fi
-    echo -n "."
-    sleep 1
-  done
-  echo "unable to connect to $db"
-  exit 1
-}
-
-bootDB ${DB}
+export DB=postgres
 
 # get all git submodule paths | print only the path without the extra info | cut the "package root" for go | deduplicate
 declare -a git_modules=($(git config --file .gitmodules --get-regexp path | awk '{ print $2 }' | cut -d'/' -f1,2 | sort -u))
