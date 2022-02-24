c4c8bdb778f18cabe5c203fe266c7da8eeab112e
Mario Kukutsov <mario.kukutsov@sap.com>
2022-02-24 06:35:44 +0000

    FIX conRateLimitLogChain

--- src/code.cloudfoundry.org/cni-wrapper-plugin/netrules/netout.go
+++ src/code.cloudfoundry.org/cni-wrapper-plugin/netrules/netout.go
@@ -232,12 +232,13 @@ func (m *NetOut) connRateLimitLogChain(forwardChainName string) (IpTablesFullCha
 	logRules := []rules.IPTablesRule{}
 	
 	if m.Conn.Logging || m.Conn.DryRun {
-		rules.NewNetOutConnRateLimitRejectLogRule(m.ContainerHandle, m.DeniedLogsPerSec),
+		logRules = append(rules.NewNetOutConnRateLimitRejectLogRule(m.ContainerHandle, m.DeniedLogsPerSec))
 	}
     
 	if !m.Conn.DryRun {
-		rules.NewNetOutDefaultRejectRule(),
+		logRules = append(rules.NewNetOutDefaultRejectRule())
 	}
+	
 	return m.netOutLogChain(forwardChainName, suffixNetOutRateLimitLog, logRules)
 }
 
