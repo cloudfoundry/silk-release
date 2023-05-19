432f153806921c2448ce6f076e7660a0a4c9eab9
Geoff Franks <gfranks@vmware.com>
2023-05-19 16:35:28 +0000

    reference CNI plugins we need in the tools package now that externalmods is gone

--- src/code.cloudfoundry.org/tools/tools.go
+++ src/code.cloudfoundry.org/tools/tools.go
@@ -5,6 +5,8 @@ package tools
 
 import (
 	_ "github.com/containernetworking/cni/plugins/test/noop"
+	_ "github.com/containernetworking/plugins/plugins/ipam/host-local"
+	_ "github.com/containernetworking/plugins/plugins/meta/bandwidth"
 )
 
 // This file imports packages that are used when running go generate, or used
