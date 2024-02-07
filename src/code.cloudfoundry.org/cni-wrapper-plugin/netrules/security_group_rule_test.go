09818e29cc29b4b9b03bc566cfcbe87b18b98231
Josh Russett <josh.russett@broadcom.com>
2024-02-07 23:00:01 +0000

    Add tests for ASGs with IPv6 addresses (Not implemented yet, but we were here anyway, so why not)
    Signed-off-by: Brandon Roberson <brandon.roberson@broadcom.com>

--- src/code.cloudfoundry.org/cni-wrapper-plugin/netrules/security_group_rule_test.go
+++ src/code.cloudfoundry.org/cni-wrapper-plugin/netrules/security_group_rule_test.go
@@ -66,6 +66,95 @@ var _ = Describe("SecurityGroupRule", func() {
 			Expect(err).To(Equal(netrules.ErrIPRangeConversionFailed))
 		})
 
+		Context("IPv6", func() {
+			It("parses a full address", func() {
+				securityGroupRule := policy_client.SecurityGroupRule{
+					Destination: "2001:0db8:0001:0000:0000:0ab9:C0A8:0102",
+				}
+
+				expectedIP := net.IP{0x20, 0x01, 0x0D, 0xB8, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x0a, 0xb9, 0xc0, 0xa8, 0x01, 0x02}
+
+				rule, err := netrules.NewRuleFromSecurityGroupRule(securityGroupRule)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(rule.Networks()).To(Equal([]netrules.IPRange{
+					{Start: expectedIP, End: expectedIP},
+				}))
+			})
+
+			DescribeTable("short-hand notation",
+				func(destination string) {
+					securityGroupRule := policy_client.SecurityGroupRule{
+						Destination: destination,
+					}
+
+					expectedIP := net.IP{0x20, 0x01, 0x0D, 0xB8, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x0a, 0xb9, 0xc0, 0xa8, 0x01, 0x02}
+
+					rule, err := netrules.NewRuleFromSecurityGroupRule(securityGroupRule)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(rule.Networks()).To(Equal([]netrules.IPRange{{Start: expectedIP, End: expectedIP}}))
+				},
+				Entry("when the ip contains all leading zeros", "2001:0db8:0001:0000:0000:0ab9:C0A8:0102"),
+				Entry("when the ip omits leading zeros", "2001:db8:1::ab9:C0A8:102"),
+			)
+
+			DescribeTable("dual-addresses (IPv6 + IPv4 address)",
+				func(destination string) {
+					securityGroupRule := policy_client.SecurityGroupRule{
+						Destination: destination,
+					}
+
+					expectedIP := net.IP{0x20, 0x01, 0x0D, 0xB8, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x0a, 0xb9, 0x01, 0x02, 0x03, 0x04}
+
+					rule, err := netrules.NewRuleFromSecurityGroupRule(securityGroupRule)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(rule.Networks()).To(Equal([]netrules.IPRange{{Start: expectedIP, End: expectedIP}}))
+				},
+				Entry("when the ip contains all leading zeros", "2001:0db8:0001:0000:0000:0ab9:1.2.3.4"),
+				Entry("when the ip omits leading zeros", "2001:db8:1::ab9:1.2.3.4"),
+			)
+
+			Context("IPv6 CIDR ranges", func() {
+				It("handles a subnet mask in the IPv4 space (up to /32)", func() {
+					securityGroupRule := policy_client.SecurityGroupRule{
+						Destination: "2001::/16",
+					}
+					expectedStartIP := net.IP{0x20, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+					expectedEndIP := net.IP{0x20, 0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
+
+					rule, err := netrules.NewRuleFromSecurityGroupRule(securityGroupRule)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(rule.Networks()).To(Equal([]netrules.IPRange{{Start: expectedStartIP, End: expectedEndIP}}))
+				})
+
+				It("handles a subnet mask in the IPv6 space (from /32 to /128)", func() {
+					securityGroupRule := policy_client.SecurityGroupRule{
+						Destination: "2001:0db8:0001:0000:0000:0ab9::/96",
+					}
+
+					expectedStartIP := net.IP{0x20, 0x01, 0x0D, 0xB8, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x0a, 0xb9, 0x00, 0x00, 0x00, 0x00}
+					expectedEndIP := net.IP{0x20, 0x01, 0x0D, 0xB8, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x0a, 0xb9, 0xff, 0xff, 0xff, 0xff}
+
+					rule, err := netrules.NewRuleFromSecurityGroupRule(securityGroupRule)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(rule.Networks()).To(Equal([]netrules.IPRange{{Start: expectedStartIP, End: expectedEndIP}}))
+				})
+			})
+
+			Context("IPv6 standard ranges", func() {
+				It("handles an IPv6 range", func() {
+					securityGroupRule := policy_client.SecurityGroupRule{
+						Destination: "2001:0db8:0001:0000:0000:0ab9:C0A8:0102-2001:0db8:0001:0000:0000:0ab9:C0A8:0405",
+					}
+					expectedStartIP := net.IP{0x20, 0x01, 0x0D, 0xB8, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x0a, 0xb9, 0xc0, 0xa8, 0x01, 0x02}
+					expectedEndIP := net.IP{0x20, 0x01, 0x0D, 0xB8, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x0a, 0xb9, 0xc0, 0xa8, 0x04, 0x05}
+
+					rule, err := netrules.NewRuleFromSecurityGroupRule(securityGroupRule)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(rule.Networks()).To(Equal([]netrules.IPRange{{Start: expectedStartIP, End: expectedEndIP}}))
+				})
+			})
+		})
+
 		Context("there are multiple destinations (in a comma-delimited list) defined in a single rule", func() {
 			It("parses two ip addresses", func() {
 				securityGroupRule := policy_client.SecurityGroupRule{
@@ -89,6 +178,18 @@ var _ = Describe("SecurityGroupRule", func() {
 					{Start: net.IPv4(1, 1, 1, 1), End: net.IPv4(1, 1, 1, 1)},
 					{Start: net.IPv4(192, 168, 0, 0).To4(), End: net.IPv4(192, 168, 0, 255).To4()},
 					{Start: net.IPv4(10, 0, 0, 1), End: net.IPv4(10, 0, 1, 10)},
+				}))
+			})
+
+			It("can parse both IPv4 and IPv6 address together", func() {
+				securityGroupRule := policy_client.SecurityGroupRule{
+					Destination: "1.1.1.1,AAAA:AAAA:AAAA:AAAA:AAAA:AAAA:AAAA:AAAA",
+				}
+				rule, err := netrules.NewRuleFromSecurityGroupRule(securityGroupRule)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(rule.Networks()).To(Equal([]netrules.IPRange{
+					{Start: net.IPv4(1, 1, 1, 1), End: net.IPv4(1, 1, 1, 1)},
+					{Start: net.IP{0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA}, End: net.IP{0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA}},
 				}))
 			})
 
