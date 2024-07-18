29154c0d32d9458144661002192b70d4c13538d6
Geoff Franks <geoff.franks@broadcom.com>
2024-07-18 15:00:12 +0000

    retry VPA's pre-start up to 15 times on failure FlushingAndRestoring
    Signed-off-by: Amelia Downs <amelia.downs@broadcom.com>

--- src/code.cloudfoundry.org/vxlan-policy-agent/cmd/pre-start/main.go
+++ src/code.cloudfoundry.org/vxlan-policy-agent/cmd/pre-start/main.go
@@ -17,6 +17,7 @@ const (
 	IngressChainName = "istio-ingress"
 	jobPrefix        = "silk-daemon-bootstrap"
 	logPrefix        = "cfnetworking"
+	MAX_RETRIES      = 15
 )
 
 func main() {
@@ -27,15 +28,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not initialize iptables adapter: %s", err)
 	}
+
 	err = PreStart(ipTablesAdapter)
 	if err != nil {
-		log.Fatalf("pre-start error: %s", err)
+		log.Fatalf("pre-start failed after %d attempts - giving up", MAX_RETRIES)
 	}
 }
 
 func PreStart(ipTablesAdapter rules.IPTablesAdapter) error {
-
-	return ipTablesAdapter.FlushAndRestore(`*filter
+	var err error
+	for i := 0; i < MAX_RETRIES; i++ {
+		err = ipTablesAdapter.FlushAndRestore(`*filter
 :INPUT ACCEPT [0:0]
 :FORWARD ACCEPT [0:0]
 :OUTPUT ACCEPT [0:0]
@@ -47,6 +50,13 @@ COMMIT
 :POSTROUTING ACCEPT [0:0]
 COMMIT
 `)
+		if err != nil {
+			log.Printf("pre-start error: %s", err)
+		} else {
+			break
+		}
+	}
+	return err
 }
 
 func createIpTablesAdapter(iptablesLockFile string) (rules.IPTablesAdapter, error) {
