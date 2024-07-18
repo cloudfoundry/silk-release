5e2464e379b252e074a0bf4332288804125d7f85
Geoff Franks <geoff.franks@broadcom.com>
2024-07-18 15:00:12 +0000

    retry VPA's pre-start up to 15 times on failure FlushingAndRestoring
    Signed-off-by: Amelia Downs <amelia.downs@broadcom.com>

--- src/code.cloudfoundry.org/vxlan-policy-agent/cmd/pre-start/main_test.go
+++ src/code.cloudfoundry.org/vxlan-policy-agent/cmd/pre-start/main_test.go
@@ -23,11 +23,37 @@ var _ = Describe("Pre-Start", func() {
 		Expect(fakeIpTables.FlushAndRestoreCallCount()).To(Equal(1))
 		Expect(fakeIpTables.FlushAndRestoreArgsForCall(0)).To(Equal("*filter\n:INPUT ACCEPT [0:0]\n:FORWARD ACCEPT [0:0]\n:OUTPUT ACCEPT [0:0]\nCOMMIT\n*nat\n:PREROUTING ACCEPT [0:0]\n:INPUT ACCEPT [0:0]\n:OUTPUT ACCEPT [0:0]\n:POSTROUTING ACCEPT [0:0]\nCOMMIT\n"))
 	})
-	It("propagates errors up when encountered", func() {
-		fakeIpTables.FlushAndRestoreReturns(fmt.Errorf("iptables borked"))
-		err := main.PreStart(fakeIpTables)
-		Expect(err).To(HaveOccurred())
-		Expect(err).To(MatchError("iptables borked"))
+	Context("when the PreStart() iptables flush and restore fails", func() {
+		BeforeEach(func() {
+			fakeIpTables.FlushAndRestoreReturns(fmt.Errorf("iptables borked"))
+		})
 
+		It("retries up to MAX_RETRIES times", func() {
+			err := main.PreStart(fakeIpTables)
+			Expect(err).To(HaveOccurred())
+			Expect(fakeIpTables.FlushAndRestoreCallCount()).To(Equal(main.MAX_RETRIES))
+
+		})
+
+		It("propagates errors up when encountered", func() {
+			err := main.PreStart(fakeIpTables)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError("iptables borked"))
+		})
+
+		Context("and eventually succeeds", func() {
+			BeforeEach(func() {
+				fakeIpTables.FlushAndRestoreReturnsOnCall(0, fmt.Errorf("iptables borked"))
+				fakeIpTables.FlushAndRestoreReturnsOnCall(1, fmt.Errorf("iptables borked"))
+				fakeIpTables.FlushAndRestoreReturnsOnCall(2, fmt.Errorf("iptables borked"))
+				fakeIpTables.FlushAndRestoreReturnsOnCall(3, nil)
+			})
+
+			It("retries and then clears the iptables rules", func() {
+				err := main.PreStart(fakeIpTables)
+				Expect(fakeIpTables.FlushAndRestoreCallCount()).To(Equal(4))
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
 	})
 })
